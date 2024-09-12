6addd724d15c35696155a6201187b01ffe785b8d
vavrinecm24 <vavrinecm24@Z00T1-02>
2024-09-12 09:17:33 +0200

    Console

--- src/AppsLab-002-ConsoleWriteLine/Program.cs
+++ src/AppsLab-002-ConsoleWriteLine/Program.cs
@@ -1,1 +1,11 @@
 ﻿Console.WriteLine("Hello, World!");
+string greetings = "Nazdááár";
+Console.WriteLine(greetings);
+
+greetings = "dovííí";
+Console.WriteLine(greetings);
+
+
+string name = "Matej";
+
+Console.WriteLine("Hello " + name);
