d6d6c667a1a756eb1f0ff8acaa0b2d32759a1b4d
vavrinecm24 <vavrinecm24@Z00T1-02>
2024-09-17 08:18:52 +0200

    005-Datatypes Done

--- src/AppsLab-005-DataTypes/Program.cs
+++ src/AppsLab-005-DataTypes/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int age = 16;
+string name = "Matej";
+char inicialky = 'M';
+bool isAdult = false;
+float accountBalance = 20.50f;
+DateTime dateTime = DateTime.Now;
+
+Console.WriteLine(name);
+Console.WriteLine(age);
+Console.WriteLine(inicialky);
+Console.WriteLine(isAdult);
+Console.WriteLine(accountBalance);
+Console.WriteLine(dateTime);
