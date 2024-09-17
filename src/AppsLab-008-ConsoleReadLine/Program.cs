ed530cf11a65f486e362bd016c127d1064db2a88
vavrinecm24 <vavrinecm24@Z00T1-02>
2024-09-17 09:08:45 +0200

    ConsoleReadLine Done :)

--- src/AppsLab-008-ConsoleReadLine/Program.cs
+++ src/AppsLab-008-ConsoleReadLine/Program.cs
@@ -2,14 +2,15 @@
 
 Console.WriteLine("Ako sa voláš?");
 string? meno = Console.ReadLine();
-Console.WriteLine("Ahoj, " + meno);
+Console.WriteLine("Ahoj, " + meno + "\n");
 
 Console.WriteLine("Koľko máš rokov?");
 string vstup = Console.ReadLine() ?? "0";
 int vek = int.Parse(vstup);
 Console.WriteLine("Máš " + vek + " rokov.");
+Console.WriteLine("");
 
 Console.WriteLine("Ake je tvoje oblubene jedlo?");
 string? jedlo = Console.ReadLine();
-Console.WriteLine("Hmmm, " + jedlo + " znie skvele!");
+Console.WriteLine($"Hmmm, " + jedlo + " znie skvele!");
 
