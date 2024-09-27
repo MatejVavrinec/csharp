aa0f29993ff49b7a60e067ea2736835b26069eec
mvavr <mvavrinec2008@gmail.com>
2024-09-27 17:11:04 +0200

    calculator

--- src/AppsLab-009-Operators/Calculator.cs
+++ src/AppsLab-009-Operators/Calculator.cs
@@ -10,7 +10,7 @@ public class Calculator
     /// </summary>
     public int Add(int a, int b)
     {
-        throw new NotImplementedException();
+        return a + b;
     }
 
     /// <summary>
@@ -18,7 +18,7 @@ public class Calculator
     /// </summary>
     public int Subtract(int a, int b)
     {
-        throw new NotImplementedException();
+        return a - b;
     }
 
     /// <summary>
@@ -26,7 +26,7 @@ public class Calculator
     /// </summary>
     public int Multiply(int a, int b)
     {
-        throw new NotImplementedException();
+        return a * b;
     }
 
     /// <summary>
@@ -34,7 +34,7 @@ public class Calculator
     /// </summary>
     public double Divide(int a, int b)
     {
-        throw new NotImplementedException();
+        return (double)a / (double)b;
     }
 
     /// <summary>
@@ -42,7 +42,7 @@ public class Calculator
     /// </summary>
     public int Remainder(int a, int b)
     {
-        throw new NotImplementedException();
+        return a % b;
     }
 
     /// <summary>
@@ -50,7 +50,7 @@ public class Calculator
     /// </summary>
     public bool AreEqual(int a, int b)
     {
-        throw new NotImplementedException();
+        return ((a == b) || (a == b)); 
     }
 
     /// <summary>
@@ -58,7 +58,7 @@ public class Calculator
     /// </summary>
     public bool AreNotEqual(int a, int b)
     {
-        throw new NotImplementedException();
+        return 
     }
 
     /// <summary>
