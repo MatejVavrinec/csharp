a4fef38e6eccbe2aa1df7c5d8d6fc6ee4526436f
mvavr <mvavrinec2008@gmail.com>
2024-10-02 18:34:01 +0200

    Aplikacia

--- Aplikacia/Program.cs
+++ Aplikacia/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,23 +11,31 @@ namespace Aplikacia
     {
         static void Main(string[] args)
         {
-            
-            Console.WriteLine("Zadajte meno");
-            string meno = Console.ReadLine();
 
-            string[] names = {meno};
-            foreach (string name in names)
+            List<string> listOfNames = new List<string>();
+            while (true)
             {
-                Console.WriteLine("Zoznam mien");
+                Console.WriteLine("\nZadajte meno:");
+                string name = Console.ReadLine();
+                Console.Clear();
+                if (name == "quit")
+                {
+                    break;
+                }
+                else
+                {
+                    listOfNames.Add(name);
+                }
+
+                Console.WriteLine("Vsetky mena v zozname:");
+                foreach (string item in listOfNames)
+                {
+                    Console.WriteLine(item);
+                }
             }
-
-
-
-            
-
-            
-
-
         }
     }
 }
+
+
+
