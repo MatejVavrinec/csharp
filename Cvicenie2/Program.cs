6e1fd0aacb283aa3f99745a4c3ae460eeab0f817
vavrinecm24 <vavrinecm24@Z00T1-02>
2024-10-03 09:05:57 +0200

    Cvicenie2

---
+++ Cvicenie2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cvicenie2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Ahoj svet";
+            Console.WriteLine(message);
+            string text = Console.ReadLine();
+            int lenght = message.Length;
+            Console.WriteLine(lenght);
+
+
+
+        }
+     
+
+
+    }
+}
