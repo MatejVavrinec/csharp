f255e9bb5d8e4b6e2b20f5878801e9ccc5551a44
mvavr <mvavrinec2008@gmail.com>
2024-10-07 20:50:15 +0200

    SamohlaskyDU

---
+++ SamohlaskyDU/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamohlaskyDU
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Zadajte vetu:");
+            string veta = Console.ReadLine();
+
+            int pocetSamohlasok = SpocitajSamohlasky(veta);
+
+            Console.WriteLine($"Pocet samohlasok vo vete: {pocetSamohlasok}");
+            Console.ReadLine();
+        }
+
+        static int SpocitajSamohlasky(string veta)
+        {
+            int pocet = 0;
+            char[] samohlasky = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'}; 
+
+            foreach (char znak in veta.ToLower())
+            {
+                if (Array.Exists(samohlasky, samohlaska => samohlaska == znak))
+                {
+                    pocet++;
+                }
+            }
+
+            return pocet;
+        }
+    }
+
+
+
+
+
+}
+    
+
