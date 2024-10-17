e83c91979bcfe8929629e2559493ae9d4aa8f013
vavrinecm24 <vavrinecm24@Z00T1-02>
2024-10-17 09:17:37 +0200

    Cvicenie_OOP_Hra

--- Cvicenie_OOP_Hra/Player.cs
+++ Cvicenie_OOP_Hra/Player.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,16 +14,63 @@ namespace Cvicenie_OOP_Hra
         public int HP { get; set; }
         public int Mana { get; set; }
         public int AttackPower { get; set; }
+        public int CritChance { get; set; }
+
 
         public void DamagePlayer(Player player)
         {
+            int damageMultiplier = 1;
+
+
+            Random random = new Random();
+            int randomNumber = random.Next(0,100);
+            if (randomNumber <= this.CritChance)
+            {
+                damageMultiplier = 2;
+                Console.WriteLine(this.Name + "dal kriticky zasah!");
+            }
+
+
+
+
+
             int HPofEnemy = player.HP;
-            int AttackOfCurrentPlayer = this.AttackPower;
+            int AttackOfCurrentPlayer = this.AttackPower * damageMultiplier;
             int HPofEnemyAfterFight = HPofEnemy - AttackOfCurrentPlayer;
             player.HP = HPofEnemyAfterFight;
 
         }
 
+        public bool Heal()
+        {
+            if (this.Mana > 0) 
+            {
+                int HPofPlayer = this.HP;
+                int ManaOfPlayer = this.Mana;
+                int HPOfPlayerAfterHeal = HPofPlayer + ManaOfPlayer;
+                this.Mana = 0;
+                this.HP = HPOfPlayerAfterHeal;
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+
+
+
+
+        }
+       
+        public void RefilMana(int newMana)
+        {
+            this.Mana += newMana;
+           
+
+            
+
+        }
 
 
         
--- Cvicenie_OOP_Hra/Program.cs
+++ Cvicenie_OOP_Hra/Program.cs
@@ -1,16 +1,18 @@
-﻿namespace Cvicenie_OOP_Hra
+﻿using System.Runtime.CompilerServices;
+
+namespace Cvicenie_OOP_Hra
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Player kladnaPostava = new Player() { Name = "Arthas", AttackPower = 10, HP = 10453, Mana = 50 };
-            Player zapornaPostava = new Player() { Name = "Illidian", AttackPower = 1, HP = 150600, Mana = 50 };
+            Player kladnaPostava = new Player() { Name = "Arthas", AttackPower = 10, HP = 100453, Mana = 50 };
+            Player zapornaPostava = new Player() { Name = "Illidian", AttackPower = 10, HP = 100600, Mana = 50 };
 
-
+            
             
            
-            
+            //attack command
             while (true)
             {
                 
@@ -20,8 +22,68 @@
                 zapornaPostava.DamagePlayer(kladnaPostava);
                 Console.WriteLine("Kladna postava HP:" + kladnaPostava.HP);
 
+
+
+
+
+
+                //pokus o uzdravenie postav
+                if (kladnaPostava.HP <= 20)
+                {
+                    bool washealed = kladnaPostava.Heal();
+                    if (washealed)
+                    {
+                        Console.WriteLine("Kladna postava bola uzdravena");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Kladna postava uz nema manu a nebola ozdravena");
+                    }
+
+                }
                 
+                if (zapornaPostava.HP <= 20)
+                {
+                    bool washealed = zapornaPostava.Heal();
+                    if (washealed)
+                    {
+                        Console.WriteLine("Zaporna postava bola uzdravena");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Zaporna postava uz nema manu a nebola ozdravena");
+                    }
+
+                }
+
                 
+
+                    
+                    Random random = new Random();
+                    int randomNumber = random.Next(0,50);
+                    if (randomNumber <= 1)
+                    {
+                        kladnaPostava.RefilMana(10);
+                        Console.WriteLine("Kladnej postave sa doplnila Mana");
+                    }
+                                                   
+
+                                       
+                    int randomNumberdva = random.Next(0,50);
+                    if (randomNumberdva <= 1)
+                    {
+                        zapornaPostava.RefilMana(10);
+                        Console.WriteLine("Zapornej postave sa doplnila Mana");
+                    }
+
+                
+
+
+
+
+
+
+                //Postava ma 0HP
                 if (kladnaPostava.HP < 0)
                 {
                     Console.WriteLine("Zabil si kladnu postavu");
@@ -33,7 +95,8 @@
                     Console.WriteLine("Zabil si zapornu postavu");
                     break;
                 }
-                
+
+
             }
                 
               
