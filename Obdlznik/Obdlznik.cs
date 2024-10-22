e468dad51e5eb307ae515879c006f9cd29a57ca4
vavrinecm24 <vavrinecm24@Z00T1-02>
2024-10-22 10:20:43 +0200

    Obdlznik

---
+++ Obdlznik/Obdlznik.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obdlznik
+{
+    public class Obdlznik
+    {
+        public int sirka { get; set; }
+
+        public int vyska { get; set; }
+        public Obdlznik(int sirka, int vyska)
+        {
+            this.sirka = sirka;
+            this.vyska = vyska;
+
+
+        }
+        public int Obsah()
+        {
+            return sirka * vyska;
+        }
+    }
+
+   
+}
---
+++ Obdlznik/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Obdlznik
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int sirka = Int32.Parse(Console.ReadLine());
+            int vyska = Int32.Parse(Console.ReadLine());
+            Obdlznik r = new Obdlznik(vyska,sirka);
+            int obsah = r.Obsah();
+            Console.WriteLine("obsah je " +obsah);
+            Obdlznik re = new Obdlznik(r.vyska * 10, r.sirka * 10);
+            int obsah2 = re.Obsah();
+            Console.WriteLine("obsah druheho obdlznika je " + obsah2);
+
+        }
+    }
+}
