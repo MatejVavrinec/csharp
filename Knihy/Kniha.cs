e78bf0afc30baf18ccb2f1c525f38c746ed69988
vavrinecm24 <vavrinecm24@Z00T1-02>
2024-11-19 09:38:38 +0100

    Kniha

---
+++ Knihy/Kniha.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Knihy
+{
+    class Kniha
+    {
+        private string nazov;
+        private int pocetstran;
+
+
+        public Kniha(string nazov, int pocetstran)
+        {
+            this.nazov = nazov;
+            this.pocetstran = pocetstran;
+        }
+
+        public string Nazov
+        {
+            get { return nazov; }
+            set { nazov = value; }
+        }
+
+        public int Pocetstran
+        {
+
+            get { return pocetstran; }
+            set {
+                if (value > 0)
+                {
+                    pocetstran = value;
+                }
+                else
+
+                    Console.WriteLine("Chyba! Pocet stran musi bzt viac ako nula!");
+
+                }
+
+            }
+
+        }
+    }
---
+++ Knihy/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Knihy
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Kniha kniha1 = new Kniha("Harry Potter", 500);
+
+            kniha1.Nazov = "Harry Potter";
+            kniha1.Pocetstran = 285;
+            Console.WriteLine($"Meno knihy je {kniha1.Nazov} a pocet stran je {kniha1.Pocetstran}");
+        }
+
+        
+    }
+}
