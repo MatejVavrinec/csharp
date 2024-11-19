98950675fdf675e2dcc54f4282aa88256059e6a8
vavrinecm24 <vavrinecm24@Z00T1-02>
2024-11-19 10:08:57 +0100

    Tim

---
+++ Tim/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Tim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tim1 tim1 = new Tim1 ("Jozo");
+            Tim1 tim2 = new Tim1("Dano");
+            Tim1 tim3 = new Tim1("Stevo");
+
+            Console.WriteLine("Celkovy pocet timov :" + Tim1.ZiskajPocettimov());
+        }
+    }
+}
---
+++ Tim/Tim1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tim
+{
+     public class Tim1
+    {
+        private string nazov { get; set; }
+        private static int pocettimov = 0;
+
+        public Tim1(string nazov)
+        {
+            this.nazov = nazov;
+            ++pocettimov;
+        }
+
+        public static int ZiskajPocettimov()
+        {
+            return pocettimov;
+        }
+        public string ZiskajNazov()
+        {
+            return nazov;
+        }
+    }
+}
