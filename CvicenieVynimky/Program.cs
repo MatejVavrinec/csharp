576dcd7ab3be4f8ae6bc3832a327ad9195442157
vavrinecm24 <vavrinecm24@Z00T1-02>
2024-11-26 11:01:25 +0100

    Vynimky

---
+++ CvicenieVynimky/Program.cs
@@ -0,0 +1,94 @@
+﻿using System.Linq.Expressions;
+
+namespace CvicenieVynimky
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Nacitavanie so subora Studenti.Csv
+
+            var data = File.ReadAllLines("studenti_large_chyba.csv");
+
+            var students = new List<Students>();
+           
+            foreach (var row in data.Skip(1))
+            {
+
+                var splits = row.Split(',');
+                try
+                {
+                    var newStudent = new Students(splits[0], splits[1], splits[2], Int32.Parse(splits[3]), "I.AI", splits[4]);
+                    students.Add(newStudent);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Chyba pri nacitani studensta:" + row);
+
+                    //Priklad na vynimku pri deleni nulou a nespravnym indexom
+                    /*
+                                    try
+                                    {
+                                        int[] deviders = { 0, 2, 3 };
+                                        int value = deviders[1];
+                                        int result = 5 / value;
+                                        Console.WriteLine(result);
+                                    }
+                                    catch (IndexOutOfRangeException ex)
+                                    {
+                                        Console.WriteLine("Chyba: Pristup mimo dosah pola.");
+                                    }
+                                    catch (DivideByZeroException ex)
+                                    {
+                                        Console.WriteLine("Chyba: Delenie nulou neni povolene");
+                                    }
+                    */
+                }
+
+
+
+                //Priklad na vznimky pri nespravnom nacitanom subore
+                /*
+                foreach (var row in data.Skip(1))
+                {
+                    var splits = row.Split(',');
+                    try
+                    {
+                        var newStudent = new Students(splits[0], splits[1], splits[2], Int32.Parse(splits[3]), "I.AI", splits[4]);
+                        students.Add(newStudent);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Chyba pri nacitani studensta:" + row);
+                    }*/
+               
+                
+                
+               /* 
+                Students[] student = new Students[]
+                {
+                  new Students("John", "Doe", 20, "Class A"){ Grades = new List<int>(){1,2,3,4,4,2,1}},
+                  new Students("Jane", "Smith", 22, "Class B"),
+                  new Students("Sam", "Brown", 19, "Class C")
+                };
+                var firstStudent = student[0];
+                while (true)
+                {
+                    try
+                    {
+                        Console.WriteLine("Zadajte znamku studentovi:");
+                        int znamka = Int32.Parse(Console.ReadLine());
+                        firstStudent.AddNewGrade(znamka);
+                        Console.WriteLine(firstStudent.Grades.Average());
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }*/
+                }
+
+            }
+
+
+        }
+    }
---
+++ CvicenieVynimky/Students.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CvicenieVynimky
+{
+    public class Students
+
+       {
+
+        public List<string> InapropriateName { get; set; } = new List<string>{"Fuck","Shit","Idiot"};
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public int Age { get; set; }
+        public string SchoolClassName { get; set; }
+        public string Gender { get; set; }
+        public List<int> Grades { get; set; }
+
+        public Students(string name, string surName, int age, string schoolClassName)
+        {
+            Name = name;
+            SurName = surName;
+            Age = age;
+            SchoolClassName = schoolClassName;
+        }
+
+        public Students(string id, string name, string surName, int age, string schoolClassName, string gender)
+        {
+            var parsed = int.TryParse(id, out int IDResult);
+            if (!parsed)
+            {
+                throw new ArgumentException("Incorrect ID");
+            }
+
+            if (name.Length == 0 || surName.Length == 0)
+            {
+                throw new ArgumentException("Incorrect name or surname");
+            }
+
+
+          
+            
+               
+            foreach (var item in InapropriateName) 
+            {
+               if (name == InapropriateName[1])
+                {
+                    throw new ArgumentException("Name contains Inappropriate word");
+                }  
+            }
+            
+
+
+        }
+        public void AddNewGrade(int grade)
+        {
+            if (grade < 1 || grade > 5)
+            {
+                throw new ArgumentException("Grade must be between 1 and 5");
+            }
+            Grades.Add(grade);
+        }
+    }
+}
