40086766982af8f095776a7b431c0ad948a81f25
vavrinecm24 <vavrinecm24@Z00T1-02>
2024-12-03 10:04:45 +0100

    OpravaPocitacovHra

---
+++ OpravaPočítačovHra/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OpravaPočítačovHra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
