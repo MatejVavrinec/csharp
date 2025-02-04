62f1bb1ea52228e9af1f712d2333d7cdd10bcc38
vavrinecm24 <mvavrinec2008@gmail.com>
2025-02-04 11:09:56 +0100

    PokemoniSaBiju

---
+++ HonkaiStarRail/GameEngine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemoniSaBiju
+{
+    internal class GameEngine
+    {
+        public Pokemon FirstPokemon {  get; set; }
+        public Pokemon SecondPokemon { get; set; }
+
+        public GameEngine()
+        {
+            FirstPokemon = new Pokemon("Pikachu" );
+            SecondPokemon = new Pokemon("Charizard");
+        }
+
+    }
+}
---
+++ HonkaiStarRail/Pokemon.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PokemoniSaBiju
+{
+    internal class Pokemon
+    {
+        public string Name { get; set; }
+
+        public int Health { get; set; }
+
+        public Pokemon(string name, int health)
+        {
+            Name = name;
+            Health = health;
+        }
+
+        public int Attack1()
+        {
+            Random rnd = new Random();
+            return rnd.Next(10, 91);
+        }
+        public int Attack2()
+        {
+            Random rnd = new Random();
+            return rnd.Next(30, 71);
+        }
+        public int Attack3()
+        {
+            Random rnd = new Random();
+            return rnd.Next(40, 60);
+        }
+        public int Heal()
+        {
+            Random rnd = new Random();
+            int healvalue = rnd.Next(20, 70);
+            Health += healvalue;
+            if (Health > 100)
+            {
+                Health = 100;
+            } 
+            return healvalue;
+            
+        }
+        
+    }
+}
